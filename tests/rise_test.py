f4e787b15216422484536f49503c87c282104d82
Yiqin <evezhang@bu.edu>
2023-11-11 23:04:37 -0500

    refector rise_test.py and add exception handling

--- tests/rise_test.py
+++ tests/rise_test.py
@@ -1,76 +1,76 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
-from webdriver_manager.chrome import ChromeDriverManager
-#from webdriver_manager.core.utils import ChromeType
-from webdriver_manager.chrome import ChromeType
+from webdriver_manager.chrome import ChromeDriverManager, ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import platform
 import sys
-import time
 
-if len(sys.argv) <= 1:
-    print("Invalid number of arguments")
-    exit(1)
+def setup_driver():
+    chrome_options = Options()
+    if platform.system() in ['Windows', 'Darwin']:
+        chrome_options.add_experimental_option('detach', True)
+    elif platform.system() == 'Linux':
+        chrome_options.add_argument('--headless')
+    else:
+        raise Exception("Unknown OS")
+    return webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install()), options=chrome_options)
 
-token = sys.argv[1]
-url = "http://127.0.0.1:8888/lab?token=" + token
-chrome_options = Options()
-
-if platform.system() == 'Windows' or platform.system() == 'Darwin':
-    chrome_options.add_experimental_option('detach', True)
-    #chrome_options.add_argument('--headless')
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install()), options=chrome_options)
-elif platform.system() == 'Linux':
-    chrome_options.add_argument('--headless') # Inorder for Action to run, it needs to be headless
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install(), chrome_type=ChromeType.GOOGLE), options=chrome_options)
-    #driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options) #kes og command changed due to vers mismatof chrome
-else:
-    print("Unknown OS")
-    exit(1)
-
-print("Accessing url: "+url)
-driver.get(url)
-
-driver.implicitly_wait(45)
-
-driver.save_screenshot('main_screenshot.png')
-print("Saved main screenshot")
+def find_element_and_click(driver, xpath):
+    try:
+        element = driver.find_element(By.XPATH, xpath)
+        element.click()
+        return element
+    except NoSuchElementException as e:
+        print(f"Error finding or clicking element: {e}")
+        return None
 
 
-main_window_handle = driver.current_window_handle #used to return to base screen for next test
+def take_screenshot(driver, filename):
+    try:
+        driver.save_screenshot(filename)
+        print(f"Saved screenshot: {filename}")
+    except Exception as e:
+        print(f"Error taking screenshot: {e}")
+
+def test_rise_extension(token):
+    driver = setup_driver()
+    url = f"http://127.0.0.1:8888/lab?token={token}"
 
 
-new_nb_button = driver.find_element(By.XPATH, '//div[@data-category="Notebook"]')
-print("Found new notebook button")
+    try:
+        driver.get(url)
+        # Main actions and interactions
+        take_screenshot(driver, 'main_screenshot.png')
+        find_element_and_click(driver, '//div[@data-category="Notebook"]')
+        take_screenshot(driver, 'new_nb_screenshot.png')
 
-new_nb_button.click()
-time.sleep(10)
+        find_element_and_click(driver, '//button[@data-command="RISE:preview"]')
+        take_screenshot(driver, 'rise_screenshot.png')
 
-driver.save_screenshot('new_nb_screenshot.png')
-print("Saved new notebook screenshot")
+        toolbar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='lm-Widget p-Widget jp-Toolbar' and @role='navigation']")))
+        if toolbar:
+            fullscreen_button = toolbar.find_element(By.XPATH, ".//button[@title='Open the slideshow in full screen']")
+            fullscreen_button.click()
+            is_fullscreen = driver.execute_script("return document.fullscreenElement !== null")
+            print("Fullscreen button is functioning: ", is_fullscreen)
+        else:
+            print("Toolbar not found")
 
+        print("RISE Extension is working!")
+    except Exception as e:
+        print(f"An error occurred during test: {e}")
+    finally:
+        # Clean up
+        driver.switch_to.window(driver.window_handles[0])
+        driver.refresh()
+        driver.quit()
 
-rise_button = driver.find_element(By.XPATH, '//button[@data-command="RISE:preview"]')
-print("Found RISE presentation button")
-rise_button.click()
-
-time.sleep(10)
-driver.save_screenshot('rise_screenshot.png')
-print("Saved rise screenshot")
-
-
-toolbar = driver.find_element(By.XPATH, "//div[@class='lm-Widget p-Widget jp-Toolbar' and @role='navigation']")
-fullscreen_button = toolbar.find_element(By.XPATH,".//button[@title='Open the slideshow in full screen']")
-fullscreen_button.click()
-is_fullscreen = driver.execute_script("return document.fullscreenElement !== null")
-print("fullscreen button is functioning")
-
-print("Rise Extension is working!")
-
-print("returning to homepage")
-
-driver.switch_to.window(main_window_handle)
-driver.refresh()
-
-
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Usage: python script.py <token>")
+    else:
+        test_rise_extension(sys.argv[1])
