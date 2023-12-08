2babe141f91be2701471fbc314da4ac7ebf8cfb8
Yiqin <evezhang@bu.edu>
2023-12-07 23:37:20 -0500

    Add close terminal in test_terminal

--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -36,12 +36,18 @@ def test_terminal(token):
     try:
         driver.get(url)
         print("Opened JupyterLab.")
-        # Wait for the Launcher button to be clickable
-        launcher_button = WebDriverWait(driver, 45).until(
-            EC.element_to_be_clickable((By.XPATH,  '//button[@data-command="launcher:create"]'))
-        )
-        launcher_button.click()
-        print("Opened Launcher.")
+        #used to return to base screen for next test
+        main_window_handle = driver.current_window_handle 
+        try:
+            # Try to find the Launcher button
+            launcher_button = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, '//button[@data-command="launcher:create"]'))
+            )
+            launcher_button.click()
+            print("Opened the Launcher.")
+        except:
+            print("Launcher button not found, skipping to Terminal.")
+
         # Click on the Terminal icon in the Launcher
         terminal_icon = WebDriverWait(driver, 30).until(
             EC.element_to_be_clickable((By.XPATH, '//div[@title="Start a new terminal session"]'))
@@ -54,15 +60,32 @@ def test_terminal(token):
         print("Terminal is ready for input.")    
         # Find terminal input and execute a command
         terminal_input.send_keys('echo Hello World' + Keys.ENTER)
-        print("Output ...")    
-        output = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'jp-Terminal')]"))
-        )
+        print("Command sent to terminal.")   
+        # Find terminal output and print it
+        output_xpath = "//div[contains(@class, 'jp-Terminal-body')]"
+        try:
+            output = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, output_xpath))
+            )
+            print("Captured terminal output:" + output.text)
+        except Exception as e:
+            print(f"An error occurred during test: {e}")
         print("Terminal test successful.")
-
+        # Close the terminal
+        close_terminal_xpath = "//div[contains(@class, 'jp-icon-hover') and contains(@class, 'lm-TabBar-tabCloseIcon')]"
+        try:
+            close_terminal_button = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, close_terminal_xpath))
+            )
+            close_terminal_button.click()
+            print("Closed Terminal.")
+        except Exception as e:
+            print("Error closing terminal: ", e)
     except Exception as e:
         print(f"An error occurred during test: {e}")
     finally:
+        driver.switch_to.window(main_window_handle)
+        driver.refresh()
         driver.quit()
 
 if __name__ == "__main__":
